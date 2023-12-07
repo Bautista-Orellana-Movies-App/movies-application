416a4d11544f5b85ab791e113ee556176e3a1926
Luis Orellana <luis.m.orellana01@gmail.com>
2023-12-07 11:51:02 -0600

    reformatted code to structure movies index and movies function js files

--- js/movies-project-functions.js
+++ js/movies-project-functions.js
@@ -1,4 +1,94 @@
-import { loadMovies} from "./movies-project-index.js";
+"use strict"
+
+//obtaining the browsers window object and adding an event listener on page load or page refresh
+function loaderAnimation() {
+    window.addEventListener('load', () => {
+        const loadMsg = document.getElementById('loading-message');
+        const body = document.getElementById('body');
+        body.style.visibility = "hidden";
+        loadMsg.style.visibility = "visible";
+    })
+}
+
+
+//Removing load animation
+function removeLoader() {
+    const loadMsg = document.getElementById('loading-message');
+    const body = document.getElementById('body');
+    loadMsg.remove()
+    body.style.visibility = "visible";
+}
+
+// imported function to load available movie selection from database
+
+const loadMovies = () => {
+    // let html = '';
+
+    fetch('http://localhost:3000/movies')
+        .then(response => response.json())
+        .then(movies => {
+            console.log(movies)
+            let moviesHTML = movies.map( movie => {
+                return `<div class="movie-card">
+                    <h4>${movie.title}</h4>
+                    <p>${movie.rating}</p>
+                    <p>${movie.description}</p>
+                    <img src=${movie.poster} alt="movie-poster">
+                    <button type="submit" class="btn btn-primary">Edit</button>
+                    <button type="submit" class="delete-btn btn btn-danger" data-movie-id="${movie.id}">Delete</button>
+
+                    </div>`;
+            })
+            // for (let i = 0; i < movies.length; i++) {
+                // console.log(movies)
+                // html += `<div class="movie-card">
+                //     <h4>${movies[i].title}</h4>
+                //     <p>${movies[i].rating}</p>
+                //     <p>${movies[i].description}</p>
+                //     <img src=${movies[i].poster} alt="movie-poster">
+                //     <button type="submit" class="btn btn-primary">Edit</button>
+                //     <button type="submit" class="delete-btn btn btn-danger" data-movie-id="${movies[i].id}">Delete</button>
+                //
+                //     </div>`;
+            // }
+
+            // const movieDiv = document.createElement("div");
+            // movieDiv.setAttribute('class', "theMovie");
+
+            const movieId = document.getElementById("movie-selection")
+            movieId.innerHTML = moviesHTML;
+
+        })
+        .catch(error => {
+            console.log(error);
+            // document.getElementById('movie-selection').innerHTML = `<p style="color:black; font-size: 30px;">Something went wrong</p>`
+        })
+        .finally(() => {
+            removeLoader();
+        })
+}
+
+function deleteButtons() {
+//event listener for "delete" button in load movies function
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const movieId = button.getAttribute('data-movie-id');
+            deleteMovie(movieId)
+        })
+    })
+}
+
+// event listener for the "Edit" button in loadMovies function
+function editMovies() {
+    const editButtons = document.querySelectorAll('.edit-button');
+    editButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const movieId = button.getAttribute('data-movie-id');
+            editMovie(movieId);
+        });
+    });
+}
 
 function clearForm() {
     let clearMovieTitle = document.getElementById('movie-title')
@@ -7,7 +97,9 @@ function clearForm() {
     movieRating.value = '';
 }
 
-export const createMovie = async (movie) => {
+
+
+const createMovie = async (movie) => {
     try {
         const url = 'http://localhost:3000/movies';
         const options = {
@@ -18,17 +110,40 @@ export const createMovie = async (movie) => {
             body: JSON.stringify(movie)
         };
         const response = await fetch(url, options);
-        return await response.json();
+
+
+        clearForm();
+
+
     } catch (error) {
         console.log(error);
-    }
-    finally {
-        loadMovies()
-        clearForm()
+    } finally {
+        loadMovies();
+
     }
 }
 
-export const updateMovies = async (id, movie) => {
+
+
+//setting up an event listener for add new movie submit button
+document.getElementById('new-movie').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let movieTitle = document.getElementById('movie-title');
+    let movieRating = document.getElementById('movie-rating');
+    //creating a new movie - img
+    let movieImg = document.getElementById('defaultImg');
+    createMovie({
+        title: movieTitle.value,
+        rating: movieRating.value,
+        poster: movieImg.value
+        // description: ,
+        // poster: ''
+    });
+    clearForm();
+
+})
+
+const updateMovies = async (id, movie) => {
     try {
         const url = `http://localhost:3000/movies/${id}`;
         const options = {
@@ -43,65 +158,32 @@ export const updateMovies = async (id, movie) => {
         return updatedMovie;
     } catch (error) {
         console.error(error);
+    } finally {
+
     }
 }
 
 
-
-export const deleteMovie = async (id) => {
+const deleteMovie = async (id) => {
     try {
         const url = `http://localhost:3000/movies/${id}`;
         const options = {
             method: 'DELETE'
         };
         const response = await fetch(url, options);
-        const deletedMovies = await response.json();
-        return deletedMovies;
+        const deletedMovie = await response.json();
+
+        // await loadMovies();
+
+        console.log(`Movie with ID ${id} deleted:`)
+
     } catch (error) {
         console.error(error);
+    } finally {
+
     }
 }
 
-
-
-// export const getMovieInfo = async (id) => {
-//     try {
-//         // Get the movie data
-//         const movieUrl = `http://localhost:3000/movies/${id}`;
-//         const moviesResponse = await fetch(movieUrl);
-//         const movie = await movieResponse.json();
-//         // Get the author data
-//         // const authorUrl = `http://localhost:3000/authors/${book.authorId}`;
-//         // const authorResponse = await fetch(authorUrl);
-//         // const author = await authorResponse.json();
-//         // Add the author to the book object
-//         // book.author = author;
-//         // Return the movie object
-//         return movie;
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// export const getAllMovies = async () => {
-//     try {
-//         // Get all the books
-//         const moviesUrl = 'http://localhost:3000/movies';
-//         const moviessResponse = await fetch(moviesUrl);
-//         const moviess = await moviesResponse.json();
-//         // Get all the authors
-//         // const authorsUrl = 'http://localhost:3000/authors';
-//         // const authorsResponse = await fetch(authorsUrl);
-//         // const authors = await authorsResponse.json();
-//         // Add the author to each book object
-//         movies.forEach(movie => {
-//             // const author = authors.find(author => author.id === book.authorId);
-//             // book.author = author;
-//         });
-//         // Return the books array
-//         return books;
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-// getMovieInfo()
+export {
+    loaderAnimation, loadMovies, updateMovies, deleteMovie, createMovie
+}
--- js/movies-project-index.js
+++ js/movies-project-index.js
@@ -1,119 +1,28 @@
 "use strict"
 
-import {createMovie, updateMovies, deleteMovie} from "./movies-project-functions.js";
-
-
-//setting up an event listener for add new movie submit button
-document.getElementById('new-movie').addEventListener('submit', function (event) {
-    event.preventDefault();
-    let movieTitle = document.getElementById('movie-title');
-    let movieRating = document.getElementById('movie-rating');
-    //creating a new movie - img
-    let movieImg = document.getElementById('defaultImg');
-    createMovie({
-        title: movieTitle.value,
-        rating: movieRating.value,
-        poster: movieImg.value
-        // description: ,
-        // poster: ''
-
-    });
-})
-
-
-//obtaining the browsers window object and adding an event listener on page load or page refresh
-window.addEventListener('load', () => {
-    const loadMsg = document.getElementById('loading-message');
-    const body = document.getElementById('body');
-    body.style.visibility = "hidden";
-    loadMsg.style.visibility = "visible";
-})
-
-function removeLoader() {
-    const loadMsg = document.getElementById('loading-message');
-    const body = document.getElementById('body');
-    loadMsg.remove()
-    body.style.visibility = "visible";
-}
-
-let html = "";
-const movieId = document.getElementById("movie-selection")
-
-
-export const loadMovies = () => {
-    fetch('http://localhost:3000/movies')
-        .then(response => response.json())
-        .then(movies => {
-            console.log(movies)
-            for (let i = 0; i < movies.length; i++) {
-                // console.log(movies)
-
-                html += `<div class="movie-card">
-            <h4>${movies[i].title}</h4>
-            <p>${movies[i].rating}</p>
-            <p>${movies[i].description}</p>
-            <img src=${movies[i].poster} alt="movie-poster">
-            
-            </div>`;
-
-
-            }
-            const movieDiv = document.createElement("div");
-            movieDiv.setAttribute('class', "theMovie");
-
-
-            // const movieId = document.getElementById("movie-selection")
-            movieId.appendChild(movieDiv).innerHTML = html;
-        })
-        .catch(error => {
-            console.log(error);
-            // document.getElementById('movie-selection').innerHTML = `<p style="color:black; font-size: 30px;">Something went wrong</p>`
-
-        })
-        .finally(() => {
-            removeLoader();
-        })
-}
-
-//initial loading of movies
-loadMovies();
-
-//clearing form values
+import {loadMovies, loaderAnimation} from "./movies-project-functions.js";
 
 
 
 
-// const searchBox = document.querySelector('#new-movie');
-// searchBox.addEventListener('submit', (e) => {
-//         e.preventDefault();
-//         const newMovieDiv = document.createElement('div')
-//         movieId.appendChild(newMovieDiv).innerHTML = `
-//         <h4>${e.target[0].value}</h4>
-//         <p>${e.target[1].value}</p>
-//         `
-//
-//         newMovieDiv.setAttribute('class', 'movie-card')
-//
-// })
+(async () => {
+
+    loaderAnimation()
+    //initial loading of movies
+    await loadMovies();
 
 
 
-// async function startAsyncOperation() {
-//     // Show loading message
-//     document.getElementById('loading-message').style.display = 'block';
-//     try {
-//         // Simulate an asynchronous operation, e.g., fetching data from an API
-//         await new Promise(resolve => setTimeout(resolve, 2000));
-//         // Perform your asynchronous operation here
-//         // Simulate the completion of the asynchronous operation
-//         console.log('Async operation completed successfully');
-//     } catch (error) {
-//         console.error('Async operation failed', error);
-//     } finally {
-//         // Hide loading message after the asynchronous operation is complete
-//         document.getElementById('loading-message').style.display = 'none';
-//     }
-// }
+
+
+
+
+
+})();
+
+
+
+
 
 
 
